3df7fa0a3d674452f30fb08ad114156b551af9b6
Ellen Burhansjah <eburhan@scc1.bu.edu>
2024-06-11 15:24:09 -0400

    fixed read_pq_file fn.

--- data_loader.py
+++ data_loader.py
@@ -16,12 +16,12 @@ def read_pq_file(file_path):
     random_num = rd.randrange(500)
 
     df = pd.read_parquet(file_path)
+    
+    stack_path = df.iloc[random_num]['stack_scat_path'] # 9 channel
+    rfv_path = df.iloc[random_num]['rfv_scat_path'] # 24 channel
+    ground_truth_path = df.iloc[random_num]['gt_path']
 
-    stack_paths = df['stack_scat_path'].tolist() # 9 channel
-    rfv_paths = df['rfv_scat_path'].tolist() # 24 channel
-    ground_truth_paths = df['gt_path'].tolist()
-
-    return stack_paths, rfv_paths, ground_truth_paths
+    return stack_path, rfv_path, ground_truth_path
 
 
 ##########
@@ -38,16 +38,16 @@ class TiffDataset(Dataset):
         '''Return length of the dataset.'''
         return len(self.stack_paths)
     
-    def __getitem__(self, index):
-        stack = tifffile.imread(self.stack_paths[index])
-        rfv = tifffile.imread(self.rfv_paths[index])
-        truth = tifffile.imread(self.truth_paths[index])
+    def __getitem__(self): # index
+        stack = tifffile.imread(self.stack_paths)
+        rfv = tifffile.imread(self.rfv_paths)
+        truth = tifffile.imread(self.truth_paths)
 
         '''Converting tiff into tensor and normalizing by dividing values by 255
         because the tiff files are saved in uint8, which indicates max value is 255
-        (each pixel value ranges from 0 to 255 for uint8)'''
+        (each pixel value ranges from 0 to 255  for uint8)'''
         stack = torch.tensor(stack, dtype=torch.float32) / 255.0
-        rfv = torch.tensor(rfv, dtype=torch.uint8) / 255.0
-        truth = torch.tensor(truth, dtype=torch.uint8) / 255.0
+        rfv = torch.tensor(rfv, dtype=torch.float32) / 255.0
+        truth = torch.tensor(truth, dtype=torch.float32) / 255.0
 
         return stack, rfv, truth
