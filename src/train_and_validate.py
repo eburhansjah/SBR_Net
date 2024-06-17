69998515c53940d3dcb8a3c2074a36fe233d829d
Ellen Burhansjah <eburhan@scc1.bu.edu>
2024-06-17 15:09:13 -0400

    in progress: working w/h  wandb sweep

--- src/train_and_validate.py
+++ src/train_and_validate.py
@@ -1,12 +1,11 @@
 import torch
 import os
-import tifffile
 import matplotlib.pyplot as plt
 
 from tqdm import tqdm
 
 
-def loss_plot_and_save(train_losses, val_losses):
+def loss_plot_and_save(run, train_losses, val_losses):
     plt.plot(train_losses, label='Training Loss')
     plt.plot(val_losses, label='Validation Loss')
     plt.xlabel('Epoch')
@@ -15,6 +14,11 @@ def loss_plot_and_save(train_losses, val_losses):
     plt.grid(True)
     plt.legend()
     plt.savefig('train_val_loss_plot.png')
+
+    run.log({"Visualization on training and validation losses": plt})
+
+    plt.close()
+
     return
 
 '''Fn. for comparing model output and ground truth'''
@@ -44,7 +48,9 @@ def compare_output_and_gt(run, gt_tensor, out_tensor, epoch):
 
     plt.suptitle(f"MIP images of gt and model output at epoch: {epoch + 1}")
 
-    run.log({"Visualization": plt})
+    run.log({"Visualization on model output and gt": plt})
+
+    plt.close()
 
     return
 
@@ -72,6 +78,7 @@ def train_and_validate(run, net, train_loader, val_loader, device, optimizer,
         train_pb = tqdm(enumerate(train_loader), 
                             total=len(train_loader), desc=f"Epoch {epoch + 1}/{num_epochs} - Training")
         
+        epoch_train_loss = []
         for i, (stack, rfv, truth) in train_pb:
             stack, rfv, truth = stack.to(device), rfv.to(device), truth.to(device)
 
@@ -93,7 +100,7 @@ def train_and_validate(run, net, train_loader, val_loader, device, optimizer,
 
             # lr_scheduler.step(epoch + i / len(train_loader))
 
-            train_loss.append(loss.item())
+            epoch_train_loss.append(loss.item())
 
             # Progress bar with updated current training loss value
             train_pb.set_postfix({"Current training loss": loss.item()})
@@ -105,6 +112,7 @@ def train_and_validate(run, net, train_loader, val_loader, device, optimizer,
         # Validation loop
         ##########
         net.eval()
+        epoch_val_loss = []
         val_pb = tqdm(enumerate(val_loader), 
                             total=len(val_loader), desc=f"Epoch {epoch + 1}/{num_epochs} - Validating")
         
@@ -120,17 +128,17 @@ def train_and_validate(run, net, train_loader, val_loader, device, optimizer,
                     fwd_output = net(rfv, stack)
                     loss = criterion(fwd_output, truth)
 
-                val_loss.append(loss.item())
+                epoch_val_loss.append(loss.item())
 
                 # Progress bar with updated current validation loss value
                 val_pb.set_postfix({"Current validation loss": loss.item()})
         
         # Calculating average train and validation loss
-        sum_train_loss = sum(train_loss)
-        sum_val_loss = sum(val_loss)
+        avg_train_loss = sum(epoch_train_loss) / len(epoch_train_loss)
+        avg_val_loss = sum(epoch_val_loss) / len(epoch_val_loss)
 
-        avg_train_loss = sum_train_loss / len(train_loader)
-        avg_val_loss = sum_val_loss / len(val_loader)
+        train_loss.append(avg_train_loss)
+        val_loss.append(avg_val_loss)
 
         print(f"Epoch [{epoch + 1}/{num_epochs}], Average Train Loss: {avg_train_loss:.4f}, Val Loss: {avg_val_loss:.4f}")
         
@@ -148,6 +156,6 @@ def train_and_validate(run, net, train_loader, val_loader, device, optimizer,
     print('Finished Training and Validating with values logged in wandb!')
 
     # Plotting training and validation loss
-    loss_plot_and_save(train_loss, val_loss)
+    loss_plot_and_save(run, train_loss, val_loss)
 
     return net
