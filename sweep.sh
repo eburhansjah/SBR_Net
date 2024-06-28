ca2f9b8b4e286bca81439c004a99ee48e63de650
Ellen Burhansjah <eburhan@scc1.bu.edu>
2024-06-28 15:41:37 -0400

    Exploring quantile loss

--- sweep.sh
+++ sweep.sh
@@ -26,8 +26,8 @@ cat << EOF > "$qsub_file_path"
 #!/bin/bash -l
 
 #$ -P tianlabdl		    # Specify SCC project name
-#$ -l h_rt=1:00:00	    # Hard time limit for the job
-#$ -pe omp 4            # Request 4 CPUs
+#$ -l h_rt=12:00:00	    # Hard time limit for the job
+#$ -pe omp 8           # Request 4 CPUs
 #$ -l gpus=1            # Request 1 GPU
 #$ -l gpu_c=6.0         # Specify the minimum GPU compute capability 
 
@@ -35,7 +35,7 @@ cat << EOF > "$qsub_file_path"
 
 #$ -j y				    # Merging error and output streams into single file
 
-#$ -t 1              # array jobs to define number of nodes/sweep agents to use on the SCC
+#$ -t 1-6              # array jobs to define number of nodes/sweep agents to use on the SCC
 
 module load python3/3.10.12
 source /projectnb/tianlabdl/eburhan/SBR_Net/.venv/bin/activate
