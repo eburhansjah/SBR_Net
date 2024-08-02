84a319ac8af159c9222c7fe794eaa977b4057d0d
Ellen Burhansjah <eburhan@scc1.bu.edu>
2024-08-02 12:07:34 -0400

    tidying up

---
+++ src/data/dataset.py
@@ -0,0 +1,204 @@
+import pandas as pd
+import random as rd
+import torch
+import tifffile 
+import numpy as np
+
+from torch.utils.data import Dataset
+
+
+##########
+# Function that reads .pq file and extract file paths for stack, rfv, and ground truth
+##########
+def read_pq_file(file_path, one_sample = False):
+    '''Make sure that they're in a list format for it to work with data loader class'''
+    df = pd.read_parquet(file_path)
+
+    if one_sample:
+        random_num = 456 # rd.randrange(500)
+        
+        stack_path = [df.iloc[random_num]['stack_scat_path']] # 9 channel
+        rfv_path = [df.iloc[random_num]['rfv_scat_path']] # 24 channel
+        ground_truth_path = [df.iloc[random_num]['gt_path']]
+
+        print("stack_path: ", stack_path)
+        print("rfv_path: ", rfv_path)
+        print("ground_truth path: ", ground_truth_path, "\n")
+    else:
+        stack_path = df['stack_scat_path'].tolist()
+        rfv_path = df['rfv_scat_path'].tolist()
+        ground_truth_path = df['gt_path'].tolist()
+
+    return stack_path, rfv_path, ground_truth_path
+
+
+##########
+# Creating class for dataset without patches
+##########
+class NoPatchDataset(Dataset):
+    def __init__(self, stack_paths, rfv_paths, truth_paths):
+        super(NoPatchDataset, self).__init__()
+        self.stack_paths = stack_paths
+        self.rfv_paths = rfv_paths
+        self.truth_paths = truth_paths
+
+    def __len__(self):
+        '''Return length of the dataset (# of patces per sample)'''
+        return len(self.stack_paths)
+    
+    def __getitem__(self, idx):
+        '''Reading .tiff files'''
+        stack = tifffile.imread(self.stack_paths[idx])
+        rfv = tifffile.imread(self.rfv_paths[idx])
+        truth = tifffile.imread(self.truth_paths[idx])
+
+        '''Convert to float32 to work with torch.tensor before normalizing.
+
+        Normalizing by dividing tensor values by 65535.0 because the 
+        tiff files are saved in uint16, which indicates max value is 65535.0
+        (each pixel value ranges from 0 to 65535  for uint16)
+
+        Note: Look at data_visualization.ipynb for more details'''
+        stack = stack.astype('float32') / 65535.0
+        rfv = rfv.astype('float32') / 65535.0
+        truth = truth.astype('float32') / 65535.0
+
+        '''Converting to tensor'''
+        stack_tensor = torch.tensor(stack, dtype=torch.float32)
+        rfv_tensor = torch.tensor(rfv, dtype=torch.float32)
+        truth_tensor = torch.tensor(truth, dtype=torch.float32)
+
+        # print(f"Shapes of NoPatchDataset | stack: {stack_tensor.shape}, rfv: {rfv_tensor.shape}, truth: {truth_tensor.shape}")
+
+        return stack_tensor, rfv_tensor, truth_tensor
+
+
+##########
+# Creating class for single patch dataset
+# Generating one random patch with consistent location and size across all images for 
+# stack, rfv, and truth
+##########
+class SinglePatchDataset(Dataset):
+    def __init__(self, stack_paths, rfv_paths, truth_paths, patch_size):
+        super(SinglePatchDataset, self).__init__()
+        self.stack_paths = stack_paths
+        self.rfv_paths = rfv_paths
+        self.truth_paths = truth_paths
+
+        self.patch_size = patch_size
+        self.patch_locations = self.generate_patch_locations()
+
+    def generate_patch_locations(self):
+        patch_locations = []
+
+        for path in self.stack_paths:
+            image = tifffile.imread(path)
+            H, W = image.shape[-2], image.shape[-1]
+
+            row_start = torch.randint(0, H - self.patch_size + 1, (1,)).item()
+            col_start = torch.randint(0, W - self.patch_size + 1, (1,)).item()
+            
+            patch_locations.append((row_start, col_start))
+        return patch_locations
+
+
+    def __len__(self):
+        '''Return number of patches per sample'''
+        return len(self.stack_paths)
+    
+    def __getitem__(self, idx): 
+        '''Reading .tiff files'''
+        stack = tifffile.imread(self.stack_paths[idx])
+        rfv = tifffile.imread(self.rfv_paths[idx])
+        truth = tifffile.imread(self.truth_paths[idx])
+
+        '''Retrieving patch location'''
+        row_start, col_start = self.patch_locations[idx]
+
+        '''Extracting patches'''
+        stack_patch = stack[:, row_start:row_start + self.patch_size, col_start:col_start + self.patch_size]
+        rfv_patch = rfv[:, row_start:row_start + self.patch_size, col_start:col_start + self.patch_size]
+        truth_patch = truth[:, row_start:row_start + self.patch_size, col_start:col_start + self.patch_size]
+
+        '''Normalizing'''
+        stack_patch = stack_patch.astype('float32') / 65535.0
+        rfv_patch = rfv_patch.astype('float32') / 65535.0
+        truth_patch = truth_patch.astype('float32') / 65535.0
+
+        '''Converting to tensor'''
+        stack_patch_tensor = torch.tensor(stack_patch, dtype=torch.float32)
+        rfv_patch_tensor = torch.tensor(rfv_patch, dtype=torch.float32)
+        truth_patch_tensor = torch.tensor(truth_patch, dtype=torch.float32)
+
+        # print(f"Shapes of PatchDataset | stack: {stack_patch_tensor.shape}, rfv: {rfv_patch_tensor.shape}, truth: {truth_patch_tensor.shape}")
+
+        return stack_patch_tensor, rfv_patch_tensor, truth_patch_tensor
+
+
+##########
+# Creating class for patch dataset
+##########
+class PatchDataset(Dataset):
+    def __init__(self, stack_paths, rfv_paths, truth_paths, patch_size):
+        super(PatchDataset, self).__init__()
+        self.stack_paths = stack_paths
+        self.rfv_paths = rfv_paths
+        self.truth_paths = truth_paths
+        self.patch_size = patch_size
+
+    def __len__(self):
+        '''Return number of patches per sample'''
+        image = tifffile.imread(self.stack_paths[0])
+        H, W = image.shape[-2], image.shape[-1]
+    
+        total_patches = (H // self.patch_size) * (W // self.patch_size) * len(self.stack_paths)
+        # print("Total patches: ", total_patches)
+        return total_patches
+    
+    def __getitem__(self, idx):
+        '''Retrieve a specific patch
+        Find which image the patch belongs to'''
+        accumulated_patches = 0
+        for i, path in enumerate(self.stack_paths):
+            image = tifffile.imread(path)
+            H, W = image.shape[-2], image.shape[-1]
+            num_patches_H = H // self.patch_size
+            num_patches_W = W // self.patch_size
+            num_patches = num_patches_H * num_patches_W
+
+            if accumulated_patches + num_patches > idx:
+                image_idx = i
+                patch_idx = idx - accumulated_patches
+                break
+            accumulated_patches += num_patches
+
+        '''Reading .tiff files'''
+        stack = tifffile.imread(self.stack_paths[image_idx])
+        rfv = tifffile.imread(self.rfv_paths[image_idx])
+        truth = tifffile.imread(self.truth_paths[image_idx])
+
+        '''Calculating row and column for the patch'''
+        patch_row = patch_idx // num_patches_W
+        patch_col = patch_idx % num_patches_W
+
+        row_start = patch_row * self.patch_size
+        col_start = patch_col * self.patch_size
+
+        '''Extracting patches'''
+        stack_patch = stack[:, row_start:row_start + self.patch_size, col_start:col_start + self.patch_size]
+        rfv_patch = rfv[:, row_start:row_start + self.patch_size, col_start:col_start + self.patch_size]
+        truth_patch = truth[:, row_start:row_start + self.patch_size, col_start:col_start + self.patch_size]
+
+        '''Normalizing'''
+        stack_patch = stack_patch.astype('float32') / 65535.0
+        rfv_patch = rfv_patch.astype('float32') / 65535.0
+        truth_patch = truth_patch.astype('float32') / 65535.0
+
+        '''Converting to tensor'''
+        stack_patch_tensor = torch.tensor(stack_patch, dtype=torch.float32)
+        rfv_patch_tensor = torch.tensor(rfv_patch, dtype=torch.float32)
+        truth_patch_tensor = torch.tensor(truth_patch, dtype=torch.float32)
+
+        # print(f"Shapes of PatchDataset | stack: {stack_patch_tensor.shape}, rfv: {rfv_patch_tensor.shape}, truth: {truth_patch_tensor.shape}")
+
+        return stack_patch_tensor, rfv_patch_tensor, truth_patch_tensor
---
+++ src/models/SBR_UNet.py
@@ -0,0 +1,146 @@
+import torch
+import torch.nn as nn
+import torch.nn.init as init
+import math
+
+"""
+Building Network architecture based on the paper:
+Robust single-shot 3D fluorescence imaging in scattering media
+with a simulator-trained neural network
+(https://doi.org/10.1364/OE.514072)
+
+The network has two brances. One branch takes in RFV input (refocused volume)
+and the other takes in stack (light-field measurements). 
+
+Modification to the SBR-Net architecture:
+Instead of having each branch as Residual Blocks, we are using U-Net architecture for each of the branches.
+"""
+##########
+# Setting up Kaiming He initialization that will be used once for all
+# Conv2d layers. Biases must be set to zero.
+# This ensures that all layers remain close to 0 and 1.
+##########
+def kaiming_he_init(layer):
+    if isinstance(layer, nn.Conv2d):
+        init.kaiming_normal_(layer.weight, mode='fan_in', nonlinearity='relu')
+
+        if layer.bias is not None:
+            init.zeros_(layer.bias)
+
+
+##########
+# U-Net Architecture class, referenced from:
+# https://towardsdatascience.com/cook-your-first-u-net-in-pytorch-b3297a844cf3
+# padding = 1 s.t. output is not cropped
+##########
+class UNet(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(UNet, self).__init__()
+
+        # Encoder section of the U-Net (downsampling)
+        self.encoder1 = self.double_conv(in_channels, 64)
+        self.encoder2 = self.double_conv(64, 128)
+        self.encoder3 = self.double_conv(128, 256)
+        self.encoder4 = self.double_conv(256, 512)
+
+        self.center = self.double_conv(512, 1024)
+
+        # Decoder section of the U-Net (upsampling)
+        self.upconv1 = nn.ConvTranspose2d(1024, 512, kernel_size=2, stride=2)
+        self.decoder1 = self.double_conv(1024, 512)
+        self.upconv2 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
+        self.decoder2 = self.double_conv(512, 256)
+        self.upconv3 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
+        self.decoder3 = self.double_conv(256, 128)
+        self.upconv4 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
+        self.decoder4 = self.double_conv(128, 64)
+
+        # Output layer
+        self.outconv = nn.Conv2d(64, out_channels, kernel_size=1)
+
+    def double_conv(self, in_channels, out_channels):
+        return nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True)
+        )
+    
+    def forward(self, x):
+        # Encoder
+        e1 = self.encoder1(x)
+        e2 = self.encoder2(nn.MaxPool2d(kernel_size=2)(e1))
+        e3 = self.encoder3(nn.MaxPool2d(kernel_size=2)(e2))
+        e4 = self.encoder4(nn.MaxPool2d(kernel_size=2)(e3))
+
+        center = self.center(nn.MaxPool2d(kernel_size=2)(e4))
+
+        # Decoder
+        d1 = self.upconv1(center)
+        d1 = torch.cat([d1, e4], dim=1)
+        d1 = self.decoder1(d1)
+
+        d2 = self.upconv2(d1)
+        d2 = torch.cat([d2, e3], dim=1)
+        d2 = self.decoder2(d2)
+
+        d3 = self.upconv3(d2)
+        d3 = torch.cat([d3, e2], dim=1)
+        d3 = self.decoder3(d3)
+
+        d4 = self.upconv4(d3)
+        d4 = torch.cat([d4, e1], dim=1)
+        d4 = self.decoder4(d4)
+
+        out = self.outconv(d4)
+
+        return out
+    
+
+##########
+# Building SBR network
+##########
+class SBR_UNet(nn.Module):
+    def __init__(self, in_channels_rfv, in_channels_stack, kernel_size=3, stride=1):
+        super(SBR_UNet, self).__init__()
+        self.rfv_conv = nn.Conv2d(in_channels=in_channels_rfv, out_channels=48, 
+                      kernel_size=kernel_size, stride=stride, padding=1)
+        self.stack_conv = nn.Conv2d(in_channels=in_channels_stack, out_channels=48, 
+                      kernel_size=kernel_size, stride=stride, padding=1)
+
+        self.rfv_unet = UNet(in_channels_rfv, 24)
+        self.stack_unet = UNet(in_channels_stack, 24)
+        # self.final_conv = nn.Conv2d(48, 24, kernel_size=1)
+
+        self.conv_48_to_48 = nn.Conv2d(in_channels=48, out_channels=48, 
+                      kernel_size=kernel_size, stride=stride, padding=1)
+        
+        self.conv_48_to_24 = nn.Conv2d(in_channels=48, out_channels=24,
+                      kernel_size=kernel_size, stride=stride, padding=1)
+        
+        self.sigmoid = nn.Sigmoid()
+    
+    def forward(self, rfv_input, stack_input):
+        # Branch 1 (RFV)
+        out_rfv = self.rfv_conv(rfv_input)
+        residual_rfv = out_rfv
+        out_rfv = self.rfv_unet(out_rfv)
+        
+        out_rfv += residual_rfv / math.sqrt(2)
+        out_rfv = self.conv_48_to_48(out_rfv)
+
+        # Branch 2 (Stack)
+        out_stack = self.stack_conv(stack_input)
+        residual_stack = out_stack
+        out_stack = self.stack_unet(out_stack)
+
+        out_stack += residual_stack / math.sqrt(2)
+        out_stack = self.conv_48_to_48(out_stack)
+
+        # Fusion point
+        out = (out_rfv + out_stack) / math.sqrt(2)
+
+        out = self.conv_48_to_24(out)
+        out = self.sigmoid(out)
+
+        return out # Shape of output should be: Bx24x224x224
